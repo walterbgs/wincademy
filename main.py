3d7bd1d14b6405b33e1b793898800afd368af696
walterbgs <112460948+walterbgs@users.noreply.github.com>
2023-04-04 14:21:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -18,18 +18,19 @@ goal_1= 54
 scorers = Player_1 +' ' +str(goal_0)+', ' + Player_2 + ' '+str(goal_1)
 print(scorers)
 # 4.Create a single string
-#report= f'{Player_1} scored in the {goal_0}nd  minute \n{Player_2} scored in the {goal_1}th minute'
+#report= f'{Player_1} scored in the {goal_0}nd minute\n{Player_2} scored in the {goal_1}th minute'
 report = Player_1+ ' scored in the '+str(goal_0)+'nd minute\n'+ Player_2+ ' scored in the '+str(goal_1)+'th minute'  
 print(report)
 '''Part 2'''
 # 1.Choose a player
-player="Ronald Koeman"
+player="Ruud Gullit"
 # 2. First name
 find_space=player.find(" ")
 first_name = player[0:find_space] 
 #first_name_find = player.find(first_name)
 print(first_name)
-last_name = player[7: ]
+#last_name = player[7: ] - aangepast 
+last_name = player[player.find(" ")+1:] 
 print(last_name)
 # 3. Last name 
 #last_name_len_1= player[7: ],player.find (last_name), 
@@ -39,10 +40,13 @@ print(last_name_len)
 name_short = (player [:1]+". "+ last_name)
 print (name_short)
 # 5. Chant
-chant = (first_name+"! ") * len(player[:5]) + (first_name+"!")
+#chant = (first_name+"! ") * len(player[:5]) + (first_name+"!")
+chant1 = (first_name+'! ')*len(first_name)
+chant = chant1[:len(chant1)-1]
 print(chant)
- #6. Goodchant
-good_chant=chant[-1: ]
-good_chant=boolean!=" "
+#6. Goodchant
+#good_chant=chant[-1: ]- aangepast
+chant_last=chant[-1: ]
+good_chant=chant_last!=" "
 print (good_chant)
 
